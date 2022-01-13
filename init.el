a6ae41f1134250f83661d3d76f6136bf3d9d7b34
Abdullah Adam <abuidreesadam@gmail.com>
2022-01-13 07:08:17 +0200

    Add GNU Emacs Config

---
+++ init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(package-selected-packages
+   '(key-chord hydra evil-collection evil general which-key helpful counsel ivy-rich rainbow-delimiters doom-themes swiper use-package ivy doom-modeline)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
